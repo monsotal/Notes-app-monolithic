b09b70c9d351865032ba3eb38d623d9d1b900975
Tal Monsonego <tal.monsonego@algosec.com>
2024-06-23 18:03:35 +0300

    Added some functionalities to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,129 @@
+import { useState } from "react";
 import "./App.css";
 
+
+
 const App = () => {
+
+  type Note = {
+    id: number;
+    title: string;
+    content: string;
+  }
+
+  const [notes, setNotes] = useState<Note[]>([
+    {
+      id: 1,
+      title: "test note 1",
+      content: "bla bla note1",
+    },
+    {
+      id: 2,
+      title: "test note 2 ",
+      content: "bla bla note2",
+    },
+    {
+      id: 3,
+      title: "test note 3",
+      content: "bla bla note3",
+    },
+    {
+      id: 4,
+      title: "test note 4 ",
+      content: "bla bla note4",
+    },
+    {
+      id: 5,
+      title: "test note 5",
+      content: "bla bla note5",
+    },
+    {
+      id: 6,
+      title: "test note 6",
+      content: "bla bla note6",
+    },
+  ]);
+
+  const [title, setTitle] = useState("");
+
+  const [content, setContent] =useState("");
+
+  const [selectedNote, setSelectedNote] = useState<Note | null>(null);
+
+  const handleAddNote = (event: React.FormEvent) => {
+    event.preventDefault();
+    console.log("title: ", title);
+    console.log("content: ", content);
+
+
+  const newNote: Note = {
+    id: notes.length + 1,
+    title: title,
+    content: content
+  }
+
+  setNotes([newNote, ...notes]);
+  setTitle("");
+  setContent("");
+
+  }
+
+  const handleNoteClick = (note:Note) => {
+    setSelectedNote(note);
+    setTitle(note.title)
+    setContent(note.content);
+  }
+
+  const handleUpdateNote = (event: React.FormEvent) => {
+    event.preventDefault();
+  
+    if (!selectedNote) {
+      return;
+    }
+  
+    const updatedNote: Note = {
+      id: selectedNote.id,
+      title: title,
+      content: content,
+    };
+  
+    const updatedNotesList = notes.map((note) => (note.id === selectedNote.id ? updatedNote : note));
+  
+    setNotes(updatedNotesList);
+    setTitle("");
+    setContent("");
+    setSelectedNote(null);
+  };
+
+
   return (
     <div className="app-container">
-      <form className="note-form">
-        <input placeholder="Title" required />
-        <textarea placeholder="Content" rows={10} required />
-
+      <form className="note-form" onSubmit={handleAddNote}>
+        <input
+          value={title}
+          onChange={(event) => setTitle(event.target.value)}
+          placeholder="Title"
+          required
+        ></input>
+        <textarea
+          value={content}
+          onChange={(event) => setContent(event.target.value)}
+          placeholder="Content"
+          rows={10}
+          required
+        ></textarea>
         <button type="submit">Add Note</button>
       </form>
       <div className="notes-grid">
-        <div className="note-item">
-          <div className="notes-header">
+        {notes.map((note) => (
+          <div key={note.id} className="note-item" onClick={() => handleNoteClick(note)}>
+            <div className="notes-header">
             <button>x</button>
-          </div>
-          <h2>Note Title</h2>
-          <p>Note content here</p>
-        </div>
+            </div>
+            <h2>{note.title}</h2>
+            <p>{note.content}</p>
+            </div>
+        ))}
       </div>
     </div>
   );
