9b5688a7d517fbcaa8114e2764c8e8caed9da18a
Tal Monsonego <tal.monsonego@algosec.com>
2024-08-30 18:23:26 +0300

    changes to start.sh script

--- start.sh
+++ start.sh
@@ -4,5 +4,12 @@
 cd /home/ubuntu/Notes-app-monolithic/notes-app-server
 pm2 start npm -- start &
 
+
+#remove the default site 
+rm /etc/nginx/sites-enabled/default
+
+#Enable Nginx configuration
+ln -s /etc/nginx/sites-available/notes-app /etc/nginx/sites-enabled/
+
 # Start the Nginx server
 nginx -g 'daemon off;'
