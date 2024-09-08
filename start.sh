267e7e9b919828cf89aa53e3833bebc0f3aa920b
Tal Monsonego <tal.monsonego@algosec.com>
2024-09-08 17:42:11 +0300

    Changed base image to alpine linux to reduce image size

--- start.sh
+++ start.sh
@@ -1,21 +1,19 @@
-#!/bin/bash
+#!/bin/sh
 
-# Start the Node.js server
+# Start the Node.js server using PM2
 cd /home/ubuntu/Notes-app-monolithic/notes-app-server
 pm2 start npm -- start &
 
+# Remove the default Nginx site (this file doesn't exist in Alpine by default, but just in case)
+rm -f /etc/nginx/conf.d/default.conf
 
-#remove the default site 
-rm /etc/nginx/sites-enabled/default
+# Enable custom Nginx configuration
+ln -s /etc/nginx/conf.d/notes-app /etc/nginx/conf.d/notes-app.conf
 
-#Enable Nginx configuration
-ln -s /etc/nginx/sites-available/notes-app /etc/nginx/sites-enabled/
-
-#Add execute permissions to the site
+# Add execute permissions to the relevant directories (if needed, although this might not be necessary for directories)
 chmod +x /home/ubuntu
 chmod +x /home/ubuntu/Notes-app-monolithic
 chmod +x /home/ubuntu/Notes-app-monolithic/notes-app-ui
 
-
-# Start the Nginx server
+# Start the Nginx server in the foreground (daemon off mode)
 nginx -g 'daemon off;'
