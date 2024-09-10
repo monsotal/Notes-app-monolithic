bdb0b9c8a8a1beac9b19614ed8c5cb3df51e159d
Tal Monsonego <tal.monsonego@algosec.com>
2024-09-10 17:37:09 +0300

    Fixed the Nginx directory structure for alpine based container

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Start the Node.js server using PM2
-cd /home/ubuntu/Notes-app-monolithic/notes-app-server
+cd /home/Notes-app-monolithic/notes-app-server
 pm2 start npm -- start &
 
 # Start the Nginx server in the foreground (daemon off mode)
